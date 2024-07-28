dac4297d2852ab5926793f491e3e00f3dd28f470
Jaiber Diaz <djaiver9@gmail.com>
2024-07-27 19:39:28 -0500

    page carrusel cursos

---
+++ src/app/courses/page.jsx
@@ -0,0 +1,37 @@
+export default function Home() {
+    return (
+    <main>
+        <div className="badge badge-outline">default</div>
+        <div className="badge badge-primary badge-outline">primary</div>
+        <div className="badge badge-secondary badge-outline">secondary</div>
+        <div className="badge badge-accent badge-outline">accent</div>
+
+        
+<div className="carousel carousel-vertical rounded-box h-96">
+  <div className="carousel-item h-full">
+    <img src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp" />
+  </div>
+  <div className="carousel-item h-full">
+    <img src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp" />
+  </div>
+  <div className="carousel-item h-full">
+    <img src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp" />
+  </div>
+  <div className="carousel-item h-full">
+    <img src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp" />
+  </div>
+  <div className="carousel-item h-full">
+    <img src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp" />
+  </div>
+  <div className="carousel-item h-full">
+    <img src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp" />
+  </div>
+  <div className="carousel-item h-full">
+    <img src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp" />
+  </div>
+</div>
+
+    </main>
+ 
+    );
+}
